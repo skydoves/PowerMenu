b561a526d9cb262aa3e5ceae602a6cfca33d1b65
skydoves <skydoves@naver.com>
2018-06-17 16:37:48 +0900

    extends AbstractPowerMenu to PowerMenu & CustomPowerMenu

--- powermenu/src/main/java/com/skydoves/powermenu/CustomPowerMenu.java
+++ powermenu/src/main/java/com/skydoves/powermenu/CustomPowerMenu.java
@@ -18,56 +18,21 @@
 package com.skydoves.powermenu;
 
 import android.arch.lifecycle.Lifecycle;
-import android.arch.lifecycle.LifecycleObserver;
 import android.arch.lifecycle.LifecycleOwner;
 import android.arch.lifecycle.OnLifecycleEvent;
 import android.content.Context;
 import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.support.v7.widget.CardView;
-import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.FrameLayout;
 import android.widget.ListView;
-import android.widget.PopupWindow;
-import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@SuppressWarnings("unchecked")
-public class CustomPowerMenu<T, E extends MenuBaseAdapter<T>> implements IMenuItem<T>, LifecycleObserver {
-
-    private View backgroundView;
-    private View menuView;
-    private CardView menuCard;
-
-    private PopupWindow backgroundWindow;
-    private PopupWindow menuWindow;
-
-    private LifecycleOwner lifecycleOwner;
-
-    private E adapter;
-
-    private ListView menuListView;
-    private OnMenuItemClickListener menuItemClickListener;
-    private OnDismissedListener onDismissedListener;
-    private LayoutInflater layoutInflater;
-
-    private View headerView;
-    private View footerView;
-
-    private boolean showBackground = true;
-    private boolean allowTouchBackground = false;
-    private boolean fixedHeight = false;
-
-    private boolean isShowing = false;
-
-    private int contentViewPadding;
+@SuppressWarnings({"WeakerAccess", "unchecked", "unused"})
+public class CustomPowerMenu<T, E extends MenuBaseAdapter<T>> extends AbstractPowerMenu<E> implements IMenuItem<T> {
 
     public CustomPowerMenu(Context context) {
         initialize(context);
@@ -116,160 +81,16 @@ public class CustomPowerMenu<T, E extends MenuBaseAdapter<T>> implements IMenuIt
         addItemList(builder.Ts);
     }
 
-    private void initialize(Context context) {
-        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        backgroundView = layoutInflater.inflate(R.layout.layout_power_background, null);
-        backgroundView.setOnClickListener(background_clickListener);
-        backgroundView.setAlpha(0.5f);
-        backgroundWindow = new PopupWindow(backgroundView, RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
-
-        menuView = layoutInflater.inflate(R.layout.layout_power_menu, null);
-        menuListView = menuView.findViewById(R.id.power_menu_listView);
-        menuWindow = new PopupWindow(menuView, FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
-        menuCard = menuView.findViewById(R.id.power_menu_card);
-        adapter = (E)(new MenuBaseAdapter<>(menuListView));
-
-        setFocusable(false);
+    @Override
+    protected void initialize(Context context) {
+        super.initialize(context);
+        this.adapter = (E)(new MenuBaseAdapter<>(menuListView));
         setOnMenuItemClickListener(onMenuItemClickListener);
-        setTouchInterceptor(onTouchListener);
-
-        contentViewPadding = ConvertUtil.convertDpToPixel(10, context);
     }
 
-    private OnMenuItemClickListener onMenuItemClickListener = new OnMenuItemClickListener<T>() {
-        @Override
-        public void onItemClick(int position, T item) {
-
-        }
-    };
-
-    private View.OnTouchListener onTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-            if (event.getAction() == MotionEvent.ACTION_OUTSIDE && !showBackground) {
-                dismiss();
-                return true;
-            }
-            return false;
-        }
-    };
-
-
-    private View.OnClickListener headerFooterClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-
-        }
-    };
-
-    private View.OnClickListener background_clickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            if(!allowTouchBackground)
-                dismiss();
-        }
-    };
-
-    public void showAsDropDown(View anchor) {
-        if(!isShowing()) {
-            showPopup(anchor);
-            menuWindow.showAsDropDown(anchor);
-        }
-    }
-
-    public void showAsDropDown(View anchor, int xOff, int yOff) {
-        if(!isShowing()) {
-            showPopup(anchor);
-            menuWindow.showAsDropDown(anchor, xOff, yOff);
-        }
-    }
-
-    public void showAsAnchorCenter(View anchor) {
-        if(!isShowing()) {
-            showPopup(anchor);
-            menuWindow.showAsDropDown(anchor,
-                    anchor.getMeasuredWidth()/2 - getContentViewWidth()/2,
-                    -anchor.getMeasuredHeight()/2 - getContentViewHeight()/2);
-        }
-    }
-
-    public void showAsAnchorLeftTop(View anchor) {
-        if(!isShowing()) {
-            showPopup(anchor);
-            menuWindow.showAsDropDown(anchor, 0, -anchor.getMeasuredHeight());
-        }
-    }
-
-    public void showAsAnchorLeftBottom(View anchor) {
-        if(!isShowing()) {
-            showPopup(anchor);
-            menuWindow.showAsDropDown(anchor, 0, -getContentViewPadding());
-        }
-    }
-
-    public void showAsAnchorRightTop(View anchor) {
-        if(!isShowing()) {
-            showPopup(anchor);
-            menuWindow.showAsDropDown(anchor, anchor.getMeasuredWidth()/2 + getContentViewWidth()/2, -anchor.getMeasuredHeight());
-        }
-    }
-
-    public void showAsAnchorRightBottom(View anchor) {
-        if(!isShowing()) {
-            showPopup(anchor);
-            menuWindow.showAsDropDown(anchor, anchor.getMeasuredWidth()/2 + getContentViewWidth()/2, -getContentViewPadding());
-        }
-    }
-
-    public void showAtCenter(View anchor) {
-        if(!isShowing()) {
-            showPopup(anchor);
-            menuWindow.showAtLocation(anchor, Gravity.CENTER, 0, 0);
-        }
-    }
-
-    public void showAtCenter(View anchor, int xOff, int yOff) {
-        if(!isShowing()) {
-            showPopup(anchor);
-            menuWindow.showAtLocation(anchor, Gravity.CENTER, xOff, yOff);
-        }
-    }
-
-    public void showAtLocation(View anchor, int xOff, int yOff) {
-        if(!isShowing()) {
-            showPopup(anchor);
-            menuWindow.showAtLocation(anchor, Gravity.NO_GRAVITY, xOff, yOff);
-        }
-    }
-
-    public void showAtLocation(View anchor, int gravity, int xOff, int yOff) {
-        if(!isShowing()) {
-            showPopup(anchor);
-            menuWindow.showAtLocation(anchor, gravity, xOff, yOff);
-        }
-    }
-
-    private void showPopup(View anchor) {
-        if(showBackground) backgroundWindow.showAtLocation(anchor, Gravity.CENTER, 0, 0);
-        isShowing = true;
-        setWidth(getContentViewWidth());
-
-        if(fixedHeight) setHeight(menuWindow.getHeight());
-        else setMeasuredHeight(getContentViewHeight());
-    }
-
-    public void dismiss() {
-        if(isShowing()) {
-            backgroundWindow.dismiss();
-            menuWindow.dismiss();
-            isShowing = false;
-            if(onDismissedListener != null)
-                onDismissedListener.onDismissed();
-        }
-    }
-
-    public boolean isShowing() {
-        return isShowing;
+    public void setOnMenuItemClickListener(OnMenuItemClickListener<T> menuItemClickListener) {
+        this.menuItemClickListener = menuItemClickListener;
+        this.menuListView.setOnItemClickListener(itemClickListener);
     }
 
     private AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
@@ -279,43 +100,12 @@ public class CustomPowerMenu<T, E extends MenuBaseAdapter<T>> implements IMenuIt
         }
     };
 
-    public void setLifecycleOwner(LifecycleOwner lifecycleOwner) {
-        lifecycleOwner.getLifecycle().addObserver(this);
-        this.lifecycleOwner = lifecycleOwner;
-    }
+    private OnMenuItemClickListener onMenuItemClickListener = new OnMenuItemClickListener<T>() {
+        @Override
+        public void onItemClick(int position, T item) {
 
-    public int getContentViewWidth() {
-        int width = menuWindow.getContentView().getWidth();
-        if(width == 0) {
-            return getMeasuredContentView().getMeasuredWidth();
-        } else {
-            return width;
         }
-    }
-
-    public int getContentViewHeight() {
-        int height = menuWindow.getContentView().getHeight();
-        if(height == 0) {
-            height += getAdapter().getContentViewHeight() + getContentViewPadding();
-            if(getHeaderView() != null) height += getHeaderView().getMeasuredHeight();
-            if (getFooterView() != null) height += getFooterView().getMeasuredHeight();
-            return height;
-        } else {
-            return height;
-        }
-    }
-
-    private View getMeasuredContentView() {
-        View contentView = menuWindow.getContentView();
-        contentView.measure(
-                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
-                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
-        return contentView;
-    }
-
-    private int getContentViewPadding() {
-        return this.contentViewPadding;
-    }
+    };
 
     @Override
     public void setListView(ListView listView) {
@@ -327,200 +117,15 @@ public class CustomPowerMenu<T, E extends MenuBaseAdapter<T>> implements IMenuIt
         return getAdapter().getListView();
     }
 
-    public void setWidth(int width) {
-        this.menuWindow.setWidth(width);
-        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) menuListView.getLayoutParams();
-        layoutParams.width = width - contentViewPadding;
-        getMenuListView().setLayoutParams(layoutParams);
-    }
-
-    public void setHeight(int height) {
-        this.fixedHeight = true;
-        this.menuWindow.setHeight(height);
-    }
-
-    private void setMeasuredHeight(int height) {
-        this.menuWindow.setHeight(height);
-        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) menuListView.getLayoutParams();
-        layoutParams.height = height - contentViewPadding;
-        getMenuListView().setLayoutParams(layoutParams);
-    }
-
-    public void setDividerHeight(int height) {
-        menuListView.setDividerHeight(height);
-    }
-
-    public void setDivider(Drawable divider) {
-        menuListView.setDivider(divider);
-    }
-
-    public void setShowBackground(boolean show) {
-        this.showBackground = show;
-    }
-
-    public void setOnMenuItemClickListener(OnMenuItemClickListener<T> menuItemClickListener) {
-        this.menuItemClickListener = menuItemClickListener;
-        this.menuListView.setOnItemClickListener(itemClickListener);
-    }
-
-    public void setOnDismissedListener(OnDismissedListener onDismissedListener) {
-        this.onDismissedListener = onDismissedListener;
-    }
-
-    public void setTouchInterceptor(View.OnTouchListener onTouchListener) {
-        this.menuWindow.setTouchInterceptor(onTouchListener);
-    }
-
-    public void setOnBackgroundClickListener(View.OnClickListener onBackgroundClickListener) {
-        this.backgroundView.setOnClickListener(onBackgroundClickListener);
-    }
-
-    public void setAnimation(MenuAnimation menuAnimation) {
-        if(menuAnimation == MenuAnimation.NONE)
-            menuWindow.setAnimationStyle(0);
-        else if(menuAnimation == MenuAnimation.DROP_DOWN)
-            menuWindow.setAnimationStyle(-1);
-        else if(menuAnimation == MenuAnimation.FADE) {
-            menuWindow.setAnimationStyle(R.style.FadeMenuAnimation);
-            backgroundWindow.setAnimationStyle(R.style.FadeMenuAnimation);
-        }
-        else if(menuAnimation == MenuAnimation.SHOWUP_BOTTOM_LEFT)
-            menuWindow.setAnimationStyle(R.style.ShowUpAnimation_BL);
-        else if(menuAnimation == MenuAnimation.SHOWUP_BOTTOM_RIGHT)
-            menuWindow.setAnimationStyle(R.style.ShowUpAnimation_BR);
-        else if(menuAnimation == MenuAnimation.SHOWUP_TOP_LEFT)
-            menuWindow.setAnimationStyle(R.style.ShowUpAnimation_TL);
-        else if(menuAnimation == MenuAnimation.SHOWUP_TOP_RIGHT)
-            menuWindow.setAnimationStyle(R.style.ShowUpAnimation_TR);
-        else if(menuAnimation == MenuAnimation.SHOW_UP_CENTER)
-            menuWindow.setAnimationStyle(R.style.ShowUpAnimation_Center);
-        else if(menuAnimation == MenuAnimation.ELASTIC_BOTTOM_LEFT)
-            menuWindow.setAnimationStyle(R.style.ElasticMenuAnimation_BL);
-        else if(menuAnimation == MenuAnimation.ELASTIC_BOTTOM_RIGHT)
-            menuWindow.setAnimationStyle(R.style.ElasticMenuAnimation_BR);
-        else if(menuAnimation == MenuAnimation.ELASTIC_TOP_LEFT)
-            menuWindow.setAnimationStyle(R.style.ElasticMenuAnimation_TL);
-        else if(menuAnimation == MenuAnimation.ELASTIC_TOP_RIGHT)
-            menuWindow.setAnimationStyle(R.style.ElasticMenuAnimation_TR);
-        else if(menuAnimation == MenuAnimation.ELASTIC_CENTER)
-            menuWindow.setAnimationStyle(R.style.ElasticMenuAnimation_Center);
-    }
-
-    public void setAnimationStyle(int style) {
-        this.menuWindow.setAnimationStyle(style);
-    }
-
-    public void setMenuRadius(float radius) {
-        this.menuCard.setRadius(radius);
-    }
-
-    public void setMenuShadow(float shadow) {
-        this.menuCard.setCardElevation(shadow);
-    }
-
-    public E getAdapter() {
-        return this.adapter;
-    }
-
-    public ListView getMenuListView() {
-        return this.menuListView;
-    }
-
-    public void setHeaderView(int layout) {
-        if(this.headerView == null) {
-            View view = layoutInflater.inflate(layout, null, false);
-            setHeaderView(view);
-        }
-    }
-
-    public void setHeaderView(View view) {
-        if(this.headerView == null) {
-            this.menuListView.addHeaderView(view);
-            this.headerView = view;
-            this.headerView.setOnClickListener(headerFooterClickListener);
-            this.headerView.measure(
-                    View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
-                    View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
-        }
-    }
-
-    public void setHeaderView(View view, Object data, boolean isSelectable) {
-        if(this.headerView == null) {
-            this.menuListView.addHeaderView(view, data, isSelectable);
-            this.headerView = view;
-            this.headerView.setOnClickListener(headerFooterClickListener);
-            this.headerView.measure(
-                    View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
-                    View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
-        }
-    }
-
-    public void setFooterView(int layout) {
-        if(this.footerView == null) {
-            View view = layoutInflater.inflate(layout, null, false);
-            setFooterView(view);
-        }
-    }
-
-    public void setFooterView(View view) {
-        if(this.footerView == null) {
-            this.menuListView.addFooterView(view);
-            this.footerView = view;
-            this.footerView.setOnClickListener(headerFooterClickListener);
-            this.footerView.measure(
-                    View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
-                    View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
-        }
-    }
-
-    public void setFooterView(View view, Object data, boolean isSelectable) {
-        if(this.footerView == null) {
-            this.menuListView.addFooterView(view, data, isSelectable);
-            this.footerView = view;
-            this.footerView.setOnClickListener(headerFooterClickListener);
-            this.footerView.measure(
-                    View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
-                    View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
-        }
-    }
-
-    public void setIsClipping(boolean isClipping) {
-        this.menuWindow.setClippingEnabled(isClipping);
-    }
-
-    public View getHeaderView() {
-        return this.headerView;
-    }
-
-    public View getFooterView() {
-        return this.footerView;
-    }
-
-    public void setSelection(int position) {
-        this.menuListView.setSelection(position);
+    @Override
+    public void setSelectedPosition(int position) {
+        if (getAdapter() != null)
+            getAdapter().setSelectedPosition(position);
     }
 
     @Override
     public int getSelectedPosition() {
         return getAdapter().getSelectedPosition();
-    }
-
-    public void setBackgroundColor(int color) {
-        backgroundView.setBackgroundColor(color);
-    }
-
-    public void setBackgroundAlpha(float alpha) {
-        backgroundView.setAlpha(alpha);
-    }
-
-    public void setFocusable(boolean focusable) {
-        menuWindow.setBackgroundDrawable(new BitmapDrawable());
-        menuWindow.setOutsideTouchable(!focusable);
-    }
-
-    public void setSelectedPosition(int position) {
-        if (getAdapter() != null)
-            getAdapter().setSelectedPosition(position);
     }
 
     @Override
@@ -559,14 +164,14 @@ public class CustomPowerMenu<T, E extends MenuBaseAdapter<T>> implements IMenuIt
             getAdapter().clearItems();
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-    public void onDestroy() {
-        dismiss();
-    }
-
     @Override
     public List<T> getItemList() {
         return getAdapter().getItemList();
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    public void onDestroy() {
+        dismiss();
     }
 
     @SuppressWarnings("unchecked")
